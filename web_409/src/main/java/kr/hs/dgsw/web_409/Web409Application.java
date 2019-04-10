c9d3ecaa10d9429afee4e46b1861be3777019fb4
tbvjaos510 <tbvjaos510@naver.com>
2019-04-10 10:12:12 +0900

    4월 9일 과제

---
+++ web_409/src/main/java/kr/hs/dgsw/web_409/Web409Application.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.web_409;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web409Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web409Application.class, args);
+    }
+
+}
